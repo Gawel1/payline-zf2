990456f7a4aa0726a44f0de19f71f20b2dcaec88
Gaël CRISPYN <gcrispyn@assessfirst.com>
2014-04-30 17:02:32 +0200

    Declare new service in module config

--- config/module.config.php
+++ config/module.config.php
@@ -1,2 +1,20 @@
 <?php
-    return [];
+use Payline\Service\PaylineAwareInterface;
+
+return [
+    'service_manager' => [
+        'invokables' => [
+            'Payline\Service\Payline' => 'Payline\Service\Payline'
+        ],
+        'aliases' => [
+            'payline' => 'Payline\Service\Payline'
+        ],
+        'initializers' => [
+            function ($instance, $sm) {
+                if($instance instanceof PaylineAwareInterface) {
+                    $instance->setClient(new \paylineSDK());
+                }
+            }
+        ]
+    ]
+];
