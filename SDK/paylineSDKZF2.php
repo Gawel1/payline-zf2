c0cfddfe6146b3a6eeea7a593e0363de22b650fc
Gaël CRISPYN <gcrispyn@assessfirst.com>
2014-05-05 12:10:35 +0200

    Set log path in paylineZF2 class

--- SDK/paylineSDKZF2.php
+++ SDK/paylineSDKZF2.php
@@ -8,14 +8,27 @@ use Zend\ServiceManager\ServiceLocatorAwareTrait;
 class paylineSDKZF2 extends \paylineSDK implements ServiceLocatorAwareInterface
 {
     use ServiceLocatorAwareTrait;
+
+    public function __construct($merchant_id, $acess_key, $proxy_host, $proxy_port, $proxy_login, $proxy_password, $production, $sm)
+    {
+        $this->setServiceLocator($sm);
+        parent::__construct($merchant_id, $acess_key, $proxy_host, $proxy_port, $proxy_login, $proxy_password, $production);
+    }
+
     /**
      * (non-PHPdoc)
      * @see paylineSDK::writeTrace()
      */
     public function writeTrace($trace)
     {
+        $logPath = null;
+        $config  = $this->getServiceLocator()->get('config');
+
+        if (isset($config['payline']['log_path'])) {
+            $logPath = $this->getServiceLocator()->get('config')['payline']['log_path'];
+        }
         if(!isset($this->paylineTrace)){
-            $this->paylineTrace = new LogZF2(date('Y-m-d',time()).'.log', $this->getServiceLocator()->get('config')['payline']['log_path']);
+            $this->paylineTrace = new LogZF2('payline-' . date('Y-m-d',time()).'.log', $logPath);
         }
         $this->paylineTrace->write($trace);
     }
