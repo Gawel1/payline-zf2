0a0a2cb7257bf454c798b99f2d521147615dd1ac
Gaël CRISPYN <gcrispyn@assessfirst.com>
2014-05-05 18:09:23 +0200

    Adding consts and doWebPayment method

--- src/Payline/Service/PaylineService.php
+++ src/Payline/Service/PaylineService.php
@@ -8,8 +8,59 @@ class PaylineService implements PaylineAwareInterface
 {
     use PaylineAwareTrait;
 
-    public function doWebPayment()
+    const CASH_PAYMENT                = 'CPT';
+    const DEFERRED_PAYMENT            = 'DIF';
+    const SEVERAL_INSTALMENTS_PAYMENT = 'NX';
+    const RECURRING_PAYMENT           = 'REC';
+
+    const EURO_CURRENCY               = 978;
+    const AMERICAN_DOLLAR_CURRENCY    = 840;
+    const SWISS_FRANC_CURRENCY        = 756;
+    const POUND_STERLING_CURRENCY     = 826;
+    const CANADIAN_DOLLAR_CURRENCY    = 124;
+
+    /**
+     * Execute web paiment and catch response
+     *
+     * @param array $params
+     *
+     * @return \pl_result
+     */
+    public function doWebPayment(array $params)
     {
-        $this->client->doWebPayment($array);
+        $config = $this->getClient()->getServiceLocator()->get('config');
+        $params['contracts'] = $config['payline']['contracts'];
+        return $this->getClient()->doWebPayment($params);
+    }
+
+    /**
+     * Get all the paiement modes of the API
+     *
+     * @return array
+     */
+    public static function getPaymentModes()
+    {
+        return [
+            self::CASH_PAYMENT,
+            self::DEFERRED_PAYMENT,
+            self::SEVERAL_INSTALMENTS_PAYMENT,
+            self::RECURRING_PAYMENT
+        ];
+    }
+
+    /**
+     * Get all the currencies
+     *
+     * @return array
+     */
+    public static function getCurrencies()
+    {
+        return [
+            self::EURO_CURRENCY,
+            self::AMERICAN_DOLLAR_CURRENCY,
+            self::SWISS_FRANC_CURRENCY,
+            self::POUND_STERLING_CURRENCY,
+            self::CANADIAN_DOLLAR_CURRENCY
+        ];
     }
 }
